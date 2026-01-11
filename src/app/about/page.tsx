40f8e04d2320bd8e6b583ba92bd44ea93685d351
Code_with_Manav <codewithmanav70@gmail.com>
2026-01-11 22:46:46 +0530

    Revamp about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,18 +3,19 @@ export default function About() {
     <main className="px-6 py-12 max-w-3xl">
       <p className="text-green-400 mb-4">manav-sonawane@portfolio:~/about$</p>
 
-      <h2 className="text-2xl font-semibold mb-4">About Me</h2>
+      <h2 className="text-7xl font-semibold mb-4">About Me</h2>
 
-      <p className="text-gray-300 leading-relaxed">
-        I’m an Information Technology undergraduate at Thadomal Shahani
-        Engineering College, Mumbai, with a strong inclination toward backend
-        and full-stack development.
+      <p className="text-green-100 text-2xl leading-relaxed">
+        I’m an I.T. Engg Student @TSEC, MUmbai, with a strong inclination toward
+        backend and full-stack development.
+        <br />
         <br />
         I’ve worked across startups and technical teams, building scalable web
         applications, RESTful APIs, and backend systems using Python, Django,
         FastAPI, Flask, and Node.js. My experience includes handling
         concurrency, authentication, database optimization, and integrating
         third-party services like payment gateways and AI APIs.
+        <br />
         <br />
         Beyond development, I’ve taken up leadership and coordination roles —
         managing teams, organizing hackathons, and contributing to college
