db39b93fc1a5bfff1846eab378d652301ae92dc8
Code_with_Manav <codewithmanav70@gmail.com>
2026-01-25 08:55:56 +0530

    bug-fix: home page contact me button not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,7 @@ export default async function Home() {
           </h1>
 
           <p className="text-gray-300 mb-6 max-w-xl text-lg sm:text-xl md:text-2xl lg:text-3xl">
-            Pursuing B.E. I.T. |<br /> Python and Web
-            Developer |
+            Pursuing B.E. I.T. |<br /> Python and Web Developer |
           </p>
 
           <ul className="text-base sm:text-lg md:text-xl text-green-200 space-y-1 mb-8">
@@ -41,9 +40,11 @@ export default async function Home() {
                 View Projects
               </button>
             </Link>
-            <button className="w-full sm:w-auto border border-gray-600 px-4 py-2 hover:border-green-500 hover:text-green-200">
-              Contact Me
-            </button>
+            <Link href="/contact">
+              <button className="w-full sm:w-auto border border-gray-600 px-4 py-2 hover:border-green-500 hover:text-green-200">
+                Contact Me
+              </button>
+            </Link>
           </div>
         </section>
 
